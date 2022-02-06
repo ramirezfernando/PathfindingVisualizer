c1259bd4fe02fc8c927ef48d186efe28a0a449a6
fearzyn <ramirez.fernando2003@gmail.com>
2022-02-05 22:21:41 -0600

    added better gui and changed node class

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import pygame, sys, time
 
 # node colors
 white = (250, 250, 250)
-red = (255, 0, 0)
-green = (0, 255, 0)
-black = (0, 0, 0)
+red = (201, 102, 87)
+green = (99, 212, 139)
+lightBlue = (99, 153, 212)
+darkBlue = (32, 78, 128)
+black = (31, 28, 28)
 
 # mouse events 
 leftClick = 1
@@ -18,15 +20,9 @@ pygame.init()
 width = 800
 screen = pygame.display.set_mode((width, width + 100))
 font = pygame.font.SysFont("Arial", 20)
-screen.fill(white)
+screen.fill(darkBlue)
 pygame.display.set_caption("Pathfinding Algorithm")
-
-
 clock = pygame.time.Clock()
-
-
-
-
 
 
 
@@ -36,10 +32,12 @@ class Node:
     def __init__(self, row, col, color):
         self.row = row
         self.col = row
-        #self.x = row * width
-        #self.y = col * width
         self.color = color
         self.neigbors = []
+        self.visited = False
+        self.prev = None
+        self.barrier = False
+
   
     def getRow(self):
         return self.row
@@ -54,11 +52,26 @@ class Node:
         return [(self.row, self.col-1), (self.row, self.col+1), (self.row-1, self.col), (self.row+1, self.col)]
         # up down left right
 
+    def updatePos(self, newRow, newCol):
+        self.row = newRow
+        self.col = newCol
+        return (self.row, self.col)
+
+    def isVisited(self):
+        return self.visited
+
+
+
 startingNode = Node(5, 5, green)
 endNode = Node(35, 35, red)
 barrierNode = Node(0, 0, black)
 
-print(startingNode.getNeibors())
+
+#print(startingNode.updatePos(10, 10))
+#print(startingNode.getRow())
+startingNode.barrier = True
+print(startingNode.barrier)
+
 class Button:
     def __init__(self, text):
         self.text = font.render(text, 1, pygame.Color("White"))
@@ -67,6 +80,9 @@ class Button:
         self.surface.blit(self.text, (0,0))
 
 dijkstrasButton = Button("Dijkstra's Algorithm")
+
+
+
 
 """
 0 1 2 . . . 800
@@ -91,7 +107,7 @@ def drawGrid(size):
         for y in range(0, size, blockSize):
             #pygame.time.delay(50)
             rect = pygame.Rect(x, y, blockSize, blockSize)
-            pygame.draw.rect(screen, black, rect, 1)
+            pygame.draw.rect(screen, lightBlue, rect, 1)
             #pygame.display.update()
 
 def drawCell(row, col, color):
@@ -107,8 +123,7 @@ def eraseCell(row, col):
     for x in range(0, (800 * 2) + 1, blockSize):
         for y in range(0, (800 * 2) + 1 , blockSize):
             if (40*row == x) and (40*col == y):
-                pygame.draw.rect(screen, white, [(row-1)  * blockSize, (col-1) * blockSize, 20, 20]) 
-
+                pygame.draw.rect(screen, darkBlue, [(row-1)  * blockSize, (col-1) * blockSize, 20, 20]) 
 
 
 
@@ -119,6 +134,11 @@ running = True
 while running:
     drawGrid(width)
     screen.blit(dijkstrasButton.surface, (20, width+20))
+    drawCell(startingNode.getRow(),startingNode.getCol(), startingNode.getColor())
+    drawCell(endNode.getRow(),endNode.getCol(), endNode.getColor())
+    #dijkstrasAlgo(startingNode.getRow(),startingNode.getCol())
+
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
@@ -127,17 +147,25 @@ while running:
             x,y = pygame.mouse.get_pos() 
             x = int(x / 20) + 1
             y = int(y / 20) + 1
-            print(x,y)
+            #print(x,y)
+            print(barrierNode.updatePos(x, y))
+            print(barrierNode.isVisited())
             drawCell(x, y, barrierNode.getColor())
             #clock.tick(30)
         
         # Erases cell on right click
-        if event.type == pygame.MOUSEBUTTONDOWN and event.button == rightClick:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == rightClick or pygame.mouse.get_pressed()[2]:
             x,y = pygame.mouse.get_pos() 
             x = int(x / 20) + 1
             y = int(y / 20) + 1
             eraseCell(x, y)
-          
-    drawCell(startingNode.getRow(),startingNode.getCol(), startingNode.getColor())
-    drawCell(endNode.getRow(),endNode.getCol(), endNode.getColor())
+
+
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == leftClick and x == 10 and y == 40:
+            print("test")
+            #clock.tick(30)
+
+        
+    #drawCell(startingNode.getRow(),startingNode.getCol(), startingNode.getColor())
+    #drawCell(endNode.getRow(),endNode.getCol(), endNode.getColor())
     pygame.display.update()
